f9e37a6853a851aa734b174237eb5c62ae09778c
Yusuke Ohmichi(maimuzo) <maimuzo@gmail.com>
2020-11-14 14:28:42 +0900

    test complete, add example data

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # 7dtd-server-status
-get server status from 7dtd telnet interface, and export CSV (for excel)
+get server status from 7dtd telnet interface, and export CSV (for excel or google spread sheet)
 
 usage: python main.py
+
+output file: ./server-status.csv
+
+see example folder, and https://docs.google.com/spreadsheets/d/1sfIx1fKc0bsO8pOyE3qKjHoqWVAgAoLzqP3H3pAVMP0/edit?usp=sharing
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import re
 
 telnet_port = 8081
-timeout_sec = 30
+timeout_sec = 60
 
 # target line
 # 2020-11-14T08:37:35 883.129 INF Time: 14.68m FPS: 20.00 Heap: 1095.0MB Max: 1117.5MB Chunks: 240 CGO: 54 Ply: 1 Zom: 4 Ent: 9 (13) Items: 1 CO: 1 RSS: 2171.2MB
@@ -15,7 +15,7 @@ repatter = re.compile(target_pattern)
 with open('server-status.csv', 'w') as f:
     writer = csv.writer(f)
     # CSV header
-    writer.writerow(['datetime', 'time from boot(min)', 'FPS', 'Heap memory(MB)', 'Max memory(MB)', 'chunks', 'CGO', 'Players', 'Zombie', 'Entity', 'Total Entity', 'Items', 'CO', 'RSS(MB)'])
+    writer.writerow(['timestamp', 'time from boot(min)', 'FPS', 'Heap memory(MB)', 'Max memory(MB)', 'chunks', 'CGO', 'Players', 'Zombie', 'Entity', 'Total Entity', 'Items', 'CO', 'RSS(MB)'])
 
     # retry loop
     while True:
@@ -28,29 +28,34 @@ with open('server-status.csv', 'w') as f:
 
                     # read line and match test
                     line = tn.read_until(b"\n", timeout=timeout_sec).decode('utf-8')
+                    print(line)
                     result = repatter.match(line)
 
                     if result is not None:
                         # pickup and write
-                        toCSV = []
-                        toCSV[0] = f"{groups(1)}/{groups(2)}/{groups(3)}T{groups(4)}:{groups(5)}:{groups(6) {groups(7)}}/{groups(2)}"
-                        toCSV[1] = groups(8) # time from boot(min)
-                        toCSV[2] = groups(9) # FPS
-                        toCSV[3] = groups(10) # Heap memory(MB)
-                        toCSV[4] = groups(11) # Max memory(MB)
-                        toCSV[5] = groups(12) # chunks
-                        toCSV[6] = groups(13) # CGO
-                        toCSV[7] = groups(14) # Players
-                        toCSV[8] = groups(15) # Zombie
-                        toCSV[9] = groups(16) # Entity
-                        toCSV[10] = groups(17) # Total Entity
-                        toCSV[11] = groups(18) # Items
-                        toCSV[12] = groups(19) # CO
-                        toCSV[13] = groups(20) # RSS(MB)
+                        g = result.groups()
+                        toCSV = [
+                            f"{g[0]}/{g[1]}/{g[2]}T{g[3]}:{g[4]}:{g[5]}",
+                            g[7], # time from boot(min)
+                            g[8], # FPS
+                            g[9], # Heap memory(MB)
+                            g[10], # Max memory(MB)
+                            g[11], # chunks
+                            g[12], # CGO
+                            g[13], # Players
+                            g[14], # Zombie
+                            g[15], # Entity
+                            g[16], # Total Entity
+                            g[17], # Items
+                            g[18], # CO
+                            g[19], # RSS(MB)
+                        ]
 
                         writer.writerow(toCSV)
-        except:
-            print("detect exception")
+                        f.flush()
+        except Exception as e:
+            print("detect exception. so disconnect telnet server.")
+            print(e)
             # wait network recovery
             time.sleep(10)
         
